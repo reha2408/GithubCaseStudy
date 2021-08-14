9aedc7854f89c54a051662ee3516319a41cfefb9
Reha Topuzoğlu <reha2408@gmail.com>
2021-08-14 20:20:07 +0300

    example chat page.

--- app/src/main/java/com/reha/casestudy/feature/jetpackcompose/ComposeActivity.kt
+++ app/src/main/java/com/reha/casestudy/feature/jetpackcompose/ComposeActivity.kt
@@ -1,38 +1,96 @@
 package com.reha.casestudy.feature.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.reha.casestudy.R
 import com.reha.casestudy.feature.jetpackcompose.ui.theme.GithubCaseStudyTheme
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GithubCaseStudyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PageUi()
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun PageUi() {
+    GithubCaseStudyTheme {
+        Column(Modifier
+            .background(MaterialTheme.colors.background)
+            .fillMaxSize()
+        ) {
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
+            MessageCard(Message("Reha", "Hey, take a look at Me!"))
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
+            MessageCard(Message("Reha", "Hey, take a look at Me!"))
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
+            MessageCard(Message("Reha", "Hey, take a look at Jetpack Compose, it's great!"))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 12.dp).fillMaxWidth()) {
+        Image(
+            painter = painterResource(R.drawable.ex_android),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
-    GithubCaseStudyTheme {
-        Greeting("Android")
-    }
+    PageUi()
 }
--- app/src/main/java/com/reha/casestudy/feature/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/reha/casestudy/feature/jetpackcompose/ui/theme/Theme.kt
@@ -1,45 +1,48 @@
 package com.reha.casestudy.feature.jetpackcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val JetchatDarkPalette = darkColors(
+    primary = Blue200,
+    primaryVariant = Blue400,
+    onPrimary = Color.Black,
+    secondary = Yellow400,
+    onSecondary = Color.Black,
+    onSurface = Color.White,
+    onBackground = Color.White,
+    error = Red300,
+    onError = Color.Black
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+private val JetchatLightPalette = lightColors(
+    primary = Blue500,
+    primaryVariant = Blue800,
     onPrimary = Color.White,
+    secondary = Yellow700,
+    secondaryVariant = Yellow800,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+    onBackground = Color.Black,
+    error = Red800,
+    onError = Color.White
 )
 
 @Composable
 fun GithubCaseStudyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    colors: Colors? = null,
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val myColors = colors ?: if (darkTheme) JetchatDarkPalette else JetchatLightPalette
 
     MaterialTheme(
-        colors = colors,
+        colors = myColors,
         typography = Typography,
         shapes = Shapes,
         content = content
