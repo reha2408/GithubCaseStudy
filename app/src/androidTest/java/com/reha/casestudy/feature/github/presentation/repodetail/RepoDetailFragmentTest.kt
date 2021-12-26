6e7180f6a0de5e6e635ce49b18fda238bff74c2d
Reha Topuzoğlu <reha2408@gmail.com>
2021-12-26 22:00:48 +0300

    ktlint fix.

--- app/src/androidTest/java/com/reha/casestudy/feature/github/presentation/repodetail/RepoDetailFragmentTest.kt
+++ app/src/androidTest/java/com/reha/casestudy/feature/github/presentation/repodetail/RepoDetailFragmentTest.kt
@@ -1,22 +1,22 @@
 package com.reha.casestudy.feature.github.presentation.repodetail
 
 import android.os.Bundle
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
-import com.reha.casestudy.feature.github.data.model.Repo
-import com.reha.casestudy.launchFragmentInHiltContainer
-import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Test
-import org.junit.runner.RunWith
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import com.reha.casestudy.R
+import com.reha.casestudy.feature.github.data.model.Repo
+import com.reha.casestudy.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
