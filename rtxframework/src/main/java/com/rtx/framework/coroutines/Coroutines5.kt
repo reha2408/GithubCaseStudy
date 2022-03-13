165131cd3f60b10c6c5b0f5a860fec4ddf87c397
Reha Topuzoğlu <reha2408@gmail.com>
2022-03-13 16:37:20 +0300

    Update Coroutines5.kt

--- rtxframework/src/main/java/com/rtx/framework/coroutines/Coroutines5.kt
+++ rtxframework/src/main/java/com/rtx/framework/coroutines/Coroutines5.kt
@@ -10,6 +10,7 @@ fun main() {
     ex15()
     ex16()
     ex17()
+    ex18()
 }
 
 /*
@@ -93,6 +94,26 @@ fun ex17() = runBlocking {
                 println("job: I'm sleeping ${i++} ...")
                 nextPrintTime += 500L
             }
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
+
+/**
+ * Closing resources with finally.
+ */
+fun ex18() = runBlocking {
+    val job = launch {
+        try {
+            repeat(1000) { i ->
+                println("job: I'm sleeping $i ...")
+                delay(500L)
+            }
+        } finally {
+            println("job: I'm running finally")
         }
     }
     delay(1300L) // delay a bit
