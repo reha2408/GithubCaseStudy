d43f0399845416e9c91fbea940772d45033216be
Reha Topuzoğlu <reha2408@gmail.com>
2022-06-06 19:04:40 +0300

    movie row's spacing is set.

--- app/src/main/java/com/reha/casestudy/extension/BindingAdapters.kt
+++ app/src/main/java/com/reha/casestudy/extension/BindingAdapters.kt
@@ -1,7 +1,10 @@
 package com.reha.casestudy.extension
 
+import android.graphics.Rect
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.reha.casestudy.R
 import com.reha.casestudy.feature.moviedb.data.model.Movie
@@ -17,6 +20,21 @@ fun ImageView.showStarIfFavorite(isFavorite: Boolean) {
 @BindingAdapter(value = ["setMovieList"])
 fun RecyclerView.setMovieList(movieList: List<Movie>?) {
     if (movieList != null) {
+        addItemDecoration(object:RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                outRect.left = 20
+                if(parent.getChildAdapterPosition(view) == movieList.lastIndex) {
+                    outRect.right = 20
+                } else {
+                    outRect.right = 0
+                }
+            }
+        })
         val bookAdapter = MovieCategoryAdapter({})
         bookAdapter.submitList(movieList)
 
