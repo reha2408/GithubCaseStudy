24abb6bae10aa2bd1afbb6e3884dd24b128364f0
Reha Topuzoğlu <reha2408@gmail.com>
2023-01-29 05:38:23 +0300

    Update LeetCode.kt

--- rtxframework/src/main/java/com/rtx/framework/coroutines/LeetCode.kt
+++ rtxframework/src/main/java/com/rtx/framework/coroutines/LeetCode.kt
@@ -1,8 +1,19 @@
 package com.rtx.framework.coroutines
 
 fun main() {
-    print(isPalindrome(121555121))
-    print(intToRoman(1991))
+    println(isPalindrome(121555121))
+
+    val org = 1994
+    val roman = intToRoman(org)
+    val int = romanToInt(roman)
+
+    println(org)
+    println(roman)
+    println(int)
+
+    val t = "cab" // "aabccccd"
+    val p = "c*a*b" // "a*bc*d"
+    println(isMatch(t,p))
 }
 
 fun isPalindrome(x: Int): Boolean {
@@ -17,31 +28,125 @@ fun isPalindrome(x: Int): Boolean {
     }
 }
 
-fun intToRoman(num: Int): String {
-    var str = ""
-    var number = num
-    val pairs = listOf(
+fun romanToInt(roman: String): Int {
+    var result = 0
+    var strRoman = roman
+
+    val romans = listOf(
         (1000 to "M"),
-        (900 to "CM"),
         (500 to "D"),
-        (400 to "CD"),
         (100 to "C"),
-        (90 to "XC"),
         (50 to "L"),
-        (40 to "XL"),
         (10 to "X"),
-        (9 to "IX"),
         (5 to "V"),
-        (4 to "IV"),
         (1 to "I")
     )
+    val specials = listOf(
+        (900 to "CM"),
+        (400 to "CD"),
+        (90 to "XC"),
+        (40 to "XL"),
+        (9 to "IX"),
+        (4 to "IV")
+    )
+
+    specials.forEach { romanPair ->
+        strRoman.windowed(romanPair.second.length) {
+            if (it == romanPair.second)
+                result += romanPair.first
+        }
+        strRoman = strRoman.replace(romanPair.second, "")
+    }
+    romans.forEach { romanPair ->
+        strRoman.windowed(romanPair.second.length) {
+            if (it == romanPair.second)
+                result += romanPair.first
+        }
+        strRoman = strRoman.replace(romanPair.second, "")
+    }
+
+    return result
+}
+
+fun intToRoman(num: Int): String {
+    val numbs = listOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4 , 1)
+    val romans = listOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+
+    var str = ""
+    var number = num
     while (number > 0) {
-        pairs.forEach {
-            while (number - it.first >= 0) {
-                number -= it.first
-                str += it.second
+        numbs.forEachIndexed { index, i ->
+            while (number - i >= 0) {
+                number -= i
+                str += romans[index]
             }
         }
     }
     return str
+}
+
+/**
+ * "aa", p = "a" -> false
+ *
+ * "aa", p = "a*" -> true
+ *
+ * "ab", p = ".*" -> true
+ *
+ */
+fun isMatch(s: String, p: String): Boolean {
+    var t = s
+    var x = p
+    val astIndices = mutableListOf<Int>()
+    x.forEachIndexed { index, c ->
+        if (c == '*')
+            astIndices.add(index)
+    }
+    astIndices.forEach {
+        x = x.replaceFirst('*', x[it - 1])
+    }
+
+    if (astIndices.isEmpty()) {
+        return t == x
+    }
+    if (x.all { it == '.' }) {
+        return true
+    }
+
+    var startIndex = 0
+    var tokens = mutableListOf<Pair<String, Boolean>>()
+    astIndices.forEachIndexed { index, sIndex ->
+        tokens.add(x.substring(startIndex, sIndex) to false)
+        tokens.add(x.substring(sIndex, sIndex + 1) to true)
+        startIndex = sIndex + 1
+
+        if (astIndices.lastIndex == index) {
+            tokens.add(x.substring(startIndex, x.length) to false)
+        }
+    }
+
+    var grandCheck = true
+    tokens.forEach {
+        if (it.second) {
+            val diffIndex = t.indexOfFirst { char ->
+                it.first.first() != char
+            }
+
+            var check = true
+            if (diffIndex != -1) {
+                t.substring(0, diffIndex).forEach { ch ->
+                    if (ch != it.first.first() && ch != '.') {
+                        check = false
+                    }
+                }
+                t = t.substring(diffIndex)
+            }
+            grandCheck = check
+        } else {
+            grandCheck = it.first == t.substring(0, it.first.length)
+            t = t.substring(it.first.length)
+        }
+        if (!grandCheck)
+            return false
+    }
+    return grandCheck
 }
